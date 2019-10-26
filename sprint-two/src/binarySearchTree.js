fcee90869e0aa70373e4d546fe49023352efcaf6
hienhihi <emails.hienn@gmail.com>
2019-10-26 12:40:52 -0700

    Finished implementation on binary trees

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,47 @@
 var BinarySearchTree = function(value) {
+  var bst = Object.create(bstMethods);
+  bst.value = value;
+  bst.left = null;
+  bst.right = null;
+  return bst;
 };
+
+var bstMethods = {};
+
+bstMethods.insert = function(value) {
+  if (value < this.value && this.left === null) {
+    this.left = BinarySearchTree(value);
+  } else if (value < this.value) {
+    this.left.insert(value);
+  }
+  if (value > this.value && this.right === null) {
+    this.right = BinarySearchTree(value);
+  } else if (value > this.value) {
+    this.right.insert(value);
+  }
+};
+
+bstMethods.contains = function(target) {
+  if (this.value === target) {
+    return true;
+  } else if (target < this.value && this.left !== null) {
+    return this.left.contains(target);
+  } else if (target > this.value && this.right !== null) {
+    return this.right.contains(target);
+  }
+  return false;
+};
+
+bstMethods.depthFirstLog = function(cb) {
+  cb(this.value);
+  if (this.left !== null) {
+    this.left.depthFirstLog(cb);
+  }
+  if (this.right !== null) {
+    this.right.depthFirstLog(cb);
+  }
+};
+
 
 
 /*
