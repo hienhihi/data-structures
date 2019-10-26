082fd84c95544063c371c019420aab069bbdd265
hienhihi <emails.hienn@gmail.com>
2019-10-25 20:08:06 -0700

    Passed linked list, working on adding item into nested tree children

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -25,7 +25,19 @@ var LinkedList = function() {
   list.contains = function(target) {
     // create variable and set to head, if var !== target, assign var to the node next to it. when we reach tail. return false
     // reach to tail means .next === this value.
-    while ()
+    var test = list.head;
+    while (test.next !== null) {
+      if (test.value === target) {
+        return true;
+      }
+      test = Node.next;
+      if (test.next === null) {
+        if (test.value === target) {
+          return true;
+        }
+      }
+    }
+    return false;
   };
 
   return list;
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,8 @@
 var Tree = function(value) {
-  var newTree = {};
+  var newTree = Object.create(treeMethods);
   newTree.value = value;
-
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
 
   return newTree;
 };
@@ -11,12 +10,41 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Node(value));
 };
 
 treeMethods.contains = function(target) {
+  if (this.value === target) {
+    return true;
+  } else {
+    for (var i = 0; i < this.children.length; i++) {
+      if (this.children[i].value === target) {
+        return true;
+      } else if (this.children[i].children.length > 0) {
+        scanChild(this.children[i], target);
+      }
+    }
+  }
+  return false;
 };
 
+var Node = function(value) {
+  var node = {};
+  node.value = value;
+  node.children = [];
 
+  return node;
+};
+
+var scanChild = function(node, target) {
+  for (var j = 0; j < node.length; j++) {
+    if (node.value === target) {
+      return true;
+    } else if (node.children.length > 0) {
+      scanChild(node.children[j], target);
+    }
+  }
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
