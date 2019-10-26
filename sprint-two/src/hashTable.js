a7594aa7fb43ed364db47634c17ffe487a1fa879
hienhihi <emails.hienn@gmail.com>
2019-10-26 15:56:10 -0700

    finished the bare minimum content

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,18 +7,39 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (this[index] === undefined) {
+    this[index] = [];
+  }
+  if (this[index].length === 0) {
+    this[index].push([k, v]);
+  } else {
+    for (let i = 0; i < this[index].length; i++) {
+      if (this[index][i][0] === k) {
+        this[index][i][1] = v;
+      } else {
+        this[index].push([k, v]);
+      }
+    }
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  for (let i = 0; i < this[index].length; i++) {
+    if (this[index][i][0] === k) {
+      return this[index][i][1];
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  for (let i = 0; i < this[index].length; i++) {
+    if (this[index][i][0] === k) {
+      this[index].splice(i, 1);
+    }
+  }
 };
-
-
-
 /*
  * Complexity: What is the time complexity of the above functions?
  */
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,32 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = []; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (!this._storage.includes(item)) {
+    this._storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
+  for (var i = 0; i < this._storage.length; i++) {
+    if (item === this._storage[i]) {
+      return true;
+    }
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  for (var i = 0; i < this._storage.length; i++) {
+    if (item === this._storage[i]) {
+      this._storage.splice(i, 1);
+    }
+  }
 };
 
 /*
